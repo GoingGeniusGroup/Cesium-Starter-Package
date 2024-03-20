77c083b5b61da279f27e5e5818664cc02d688e3e
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-03-20 17:40:18 +0545

    v-2.1

--- next.config.js
+++ next.config.js
@@ -1,24 +1,58 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
+const cesiumSource = 'path/to/cesium/source'; // Update this with the actual path to your Cesium source directory
+
 const nextConfig = {
   webpack: (config, { webpack, isServer }) => {
     const projectRoot = process.cwd();
+    
+    // Add your webpack rule here
+    config.module.rules.push({
+      test: /\.js$/,
+      enforce: 'pre',
+      include: path.resolve(__dirname, cesiumSource),
+      use: [{
+        loader: 'strip-pragma-loader',
+        options: {
+          pragmas: {
+            debug: false
+          }
+        }
+      }]
+    });
+
     if (!isServer) {
       config.plugins.push(
         new CopyWebpackPlugin({
           patterns: [
             {
-              from: '../public/Cesium/Workers',
+              from: path.join(
+                projectRoot,
+                'node_modules/cesium/Build/Cesium/Workers'
+              ),
+              to: '../public/Cesium/Workers',
             },
             {
-              from: '../public/Cesium/ThirdParty',
+              from: path.join(
+                projectRoot,
+                'node_modules/cesium/Build/Cesium/ThirdParty'
+              ),
+              to: '../public/Cesium/ThirdParty',
             },
             {
-              from: '../public/Cesium/Assets',
+              from: path.join(
+                projectRoot,
+                'node_modules/cesium/Build/Cesium/Assets'
+              ),
+              to: '../public/Cesium/Assets',
             },
             {
-              from: '../public/Cesium/Widgets',
+              from: path.join(
+                projectRoot,
+                'node_modules/cesium/Build/Cesium/Widgets'
+              ),
+              to: '../public/Cesium/Widgets',
             },
           ],
         })
