6b2140b6b20224179942a0748dc0fec8df9c86f4
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-26 14:55:27 +0545

    added glass morphism

--- app/component/testdiv/page.tsx
+++ app/component/testdiv/page.tsx
@@ -1,20 +1,16 @@
-import QRToggle from "../qr-modal-toggle/page";
-
 const TestDiv = () => {
   return (
     <>
-      <div className="w-1/3 absolute">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg p-6">
-          <div className="h-48 rounded bg-gray-300"></div>
-          <div className="px-6 py-4">
-            <div className="h-6 rounded bg-gray-300 mb-2"></div>
-            <div className="h-4 bg-gray-300 w-2/3"></div>
+      <div className="w-1/4 absolute bg-black rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-slate-800 flex flex-col justify-center ml-4 mt-4 hover:bg-opacity-25 shadow-lg shadow-blue-800 hover:shadow-blue-700 ">
+        <div className="p-4 rounded overflow-hidden shadow-lg ">
+          <div className="h-48 rounded bg-gray-300" />
+          <div className="py-4">
+            <div className="h-6 rounded bg-gray-300 mb-2" />
+            <div className="h-4 bg-gray-300 w-2/3" />
           </div>
-          <div className="px-6 rounded pt-4 pb-2">
-            <div className="h-4 rounded bg-gray-300 w-1/4 mb-2"></div>
-            <div className="h-4 rounded bg-gray-300 w-1/2"></div>
-
-            <QRToggle />
+          <div className="rounded pt-4 pb-2">
+            <div className="h-4 rounded bg-gray-300 w-1/4 mb-2" />
+            <div className="h-4 rounded bg-gray-300 w-1/2" />
           </div>
         </div>
       </div>
